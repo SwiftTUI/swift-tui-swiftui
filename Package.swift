a2450e60e3d09b9ed64633dfc820a55ac0d15d93
adamz <adam@zethrae.us>
2026-07-22 04:40:46 -0400

    chore: release 0.1.15

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SwiftTUI/swift-tui.git",
-      exact: "0.1.13"
+      exact: "0.1.15"
     )
   ],
   targets: [
