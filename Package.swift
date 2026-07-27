572e79ab30db007af1bc664620d0287cc6c1a660
adamz <adam@zethrae.us>
2026-07-27 11:25:39 -0700

    chore: bump to 0.3.8
    Lockstep org release: consume the swift-tui 0.3.8 tag.

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SwiftTUI/swift-tui.git",
-      exact: "0.3.4"
+      exact: "0.3.8"
     )
   ],
   targets: [
