0df8a283d04716585ae59fcdf9876450901a0f74
adamz <adam@zethrae.us>
2026-07-29 00:53:30 -0700

    chore: release 0.4.1

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SwiftTUI/swift-tui.git",
-      exact: "0.4.0"
+      exact: "0.4.1"
     )
   ],
   targets: [
