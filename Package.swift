e3b66b74391b3746fdd07ea3477ab5215e2ac84e
adamz <adam@zethrae.us>
2026-08-15 09:53:26 -0700

    0.8.10

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SwiftTUI/swift-tui.git",
-      exact: "0.8.9"
+      exact: "0.8.10"
     )
   ],
   targets: [
